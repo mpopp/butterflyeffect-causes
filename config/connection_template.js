7113a60608eee3decac70261e88f5e9b043098e7
Matthias <popp.mat@gmail.com>
2016-04-21 20:58:58 +0200

    Finally added liveloading connection and some listeners

--- config/connection_template.js
+++ config/connection_template.js
@@ -1,9 +1,8 @@
 /*
-Config object to use for connecting to the desired beam account.
-You can retrieve all the ids when calling https://beam.pro/api/v1/users/current from the browser
+copy that file and rename the copy to "connection.js". Use the copy to add you own credentials.
 */
 module.exports = {
-    user: 'user'; //username of the beam account
-    password: 'password'; //password of the beam account
-    channel: 'channel'; //channel name of the beam account
+    user: 'user', //username of the beam account
+    password: 'password', //password of the beam account
+    channel: 'channel' //channel name of the beam account
 };
--- main.js
+++ main.js
@@ -9,8 +9,112 @@ var userID = 0;
 var channelID = 0;
 
 
+var chatListeners = [
+    //count mooses and trigger at 5!!!
+    {
+        mooseCount: 0
+        , alreadyTriggered: false
+        , execute: function (data) {
+            if (data.fullMessage.indexOf('!moose') > -1) {
+                this.mooseCount++;
+            }
+            if (this.mooseCount === 2 && !this.alreadyTriggered) {
+                //later on there will be awesome stuff when this happens!!!
+                console.log("THE GREAT MOOSE IS PLEASED!!!!");
+                this.alreadyTriggered = true;
+                //think about either resetting the counter here, or disabling the listener completely
+            }
+        }
+    }
 
-//TODO build up a listener list here
+
+
+
+
+
+    
+    , {
+        wordToWatch: 'banana'
+        , execute: function (data) {
+            if (data.fullMessage.toLowerCase().indexOf(this.wordToWatch) > -1) {
+                console.log("DID SOMEONE SAY " + this.wordToWatch.toUpperCase() + "?");
+                //call something that triggers a notification!
+            }
+        }
+    }
+
+
+
+
+
+    
+    , {
+        emoticonToWatch: ':cactus'
+        , execute: function (data) {
+            for (var i = 0; i < data.message.message.length; i++) {
+                var m = data.message.message;
+                if (m[i].type === 'emoticon' && m[i].text === this.emoticonToWatch) {
+                    console.log("OMG BEWARE OF THE " + this.emoticonToWatch + " (" + this.emoticonToWatch.substring(1) + ")");
+                }
+            }
+        }
+    }
+    , {
+        urlToWatch: 'beam.pro/gamingdaddies'
+        , execute: function (data) {
+            for (var i = 0; i < data.message.message.length; i++) {
+                var m = data.message.message;
+                if (m[i].type === 'link' && m[i].text === this.urlToWatch) {
+                    console.log("WOAH .. that url: " + this.urlToWatch);
+                }
+            }
+        }
+    }
+
+]
+var chatPreprocessors = [
+    {
+        //prettify
+        execute: function (data) { //prettify all the stuffz!!!!
+            //console.log(data);
+            //console.log(data.message.message);
+            var concatMessage = "";
+            for (var i = 0; i < data.message.message.length; i++) {
+                var m = data.message.message;
+                concatMessage += m[i].type === 'emoticon' || m[i].type === 'link' ? m[i].text : m[i].data;
+            }
+            console.log(data.user_name + " wrote: " + //for testing reasons
+                concatMessage);
+            data.fullMessage = concatMessage;
+        }
+    }
+];
+
+
+
+//THIS STUFF DOES THE LIVELOADING TRACKING
+
+var WsabiClientBacon = require('wsabi-client-bacon');
+var client = new WsabiClientBacon("wss://realtime.beam.pro/socket.io/");
+var channelName = "GamingDaddies";
+
+client.socket.on("open", function () {
+    // Send requests to the server with get, put, post, delete, or request.
+    client.get("/api/v1/channels/" + channelName).then(function (res) {
+        // Subscribe to a live event
+        client.live('channel:' + res.id + ':followed').then(function (res) {
+            res.onValue(function (value) {
+                console.log('Follow/Unfollow', value);
+                //TODO: DO SOMETHING USEFUL HERE (call butterflyeffect-effects)
+            })
+        });
+    });
+});
+
+//-----------------------------------------
+
+
+//CHAT TRACKING
 
 beam.use('password', {
     username: conConf.user
@@ -27,15 +131,17 @@ beam.use('password', {
     return socket.call('auth', [channelID, userID, data.authkey]);
 }).then(function () {
     console.log('You are now authenticated!');
-    socket.get('beam.pro/api/v1/live').then(function (result) {
-        console.log(result);
-    });
 
     socket.on('ChatMessage', function (data) {
-        //console.log(data);
-        //console.log(data.message.message);
-        //TODO iterate through the listener list and execute them!
-        // hand in data at least... mby even add data AND message separately
+        for (var i = 0; i < chatPreprocessors.length; i++) {
+            var preprocessor = chatPreprocessors[i];
+            preprocessor.execute(data)
+        }
+
+        for (var i = 0; i < chatListeners.length; i++) {
+            var listener = chatListeners[i];
+            listener.execute(data)
+        }
     });
 }).catch(function (err) {
     //If this is a failed request, don't log the entire request. Just log the body
